e082baa4e0c8b7c51515ab774bdc2c42b7a5afb8
Maksym <simbanyan98@gmail.com>
2025-03-05 16:20:23 +0100

    Fixed images path for deploy

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let cards = [
-  { id: 1, name: "Card 1", img: "/memory-game/images/clown.png" },
-  { id: 2, name: "Card 2", img: "/memory-game/images/heart.png" },
-  { id: 3, name: "Card 3", img: "/memory-game/images/knight.png" },
-  { id: 4, name: "Card 4", img: "/memory-game/images/pie.png" },
-  { id: 5, name: "Card 5", img: "/memory-game/images/skull.png" },
-  { id: 6, name: "Card 6", img: "/memory-game/images/smile.png" },
-  { id: 7, name: "Card 7", img: "/memory-game/images/clown.png" },
-  { id: 8, name: "Card 8", img: "/memory-game/images/heart.png" },
-  { id: 9, name: "Card 9", img: "/memory-game/images/knight.png" },
-  { id: 10, name: "Card 10", img: "/memory-game/images/pie.png" },
-  { id: 11, name: "Card 11", img: "/memory-game/images/skull.png" },
-  { id: 12, name: "Card 12", img: "/memory-game/images/smile.png" },
+  { id: 1, name: "Card 1", img: "images/clown.png" },
+  { id: 2, name: "Card 2", img: "images/heart.png" },
+  { id: 3, name: "Card 3", img: "images/knight.png" },
+  { id: 4, name: "Card 4", img: "images/pie.png" },
+  { id: 5, name: "Card 5", img: "images/skull.png" },
+  { id: 6, name: "Card 6", img: "images/smile.png" },
+  { id: 7, name: "Card 7", img: "images/clown.png" },
+  { id: 8, name: "Card 8", img: "images/heart.png" },
+  { id: 9, name: "Card 9", img: "images/knight.png" },
+  { id: 10, name: "Card 10", img: "images/pie.png" },
+  { id: 11, name: "Card 11", img: "images/skull.png" },
+  { id: 12, name: "Card 12", img: "images/smile.png" },
 ];
 let startButton = document.querySelector("#start-btn");
 startButton.addEventListener("click", function () {
@@ -26,7 +26,7 @@ startButton.addEventListener("click", function () {
     cardContainer.appendChild(cardInner);
     let cardFront = document.createElement("div");
     cardFront.className = "card-front";
-    cardFront.innerHTML = `<img class="image-cover" src="/memory-game/images/card-back.png">`;
+    cardFront.innerHTML = `<img class="image-cover" src="images/card-back.png">`;
     cardInner.appendChild(cardFront);
     let cardBack = document.createElement("div");
     cardBack.className = "card-back";
